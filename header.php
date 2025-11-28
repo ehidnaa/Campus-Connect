9406fb9e27e2cd3293fcb02ee7b4279a8104bcea
Mariia Chalyk <mari.sblle@gmail.com>
2025-11-28 11:02:33 +0000

    Add favicon to meet project requirements

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ $currentTheme = $_COOKIE['theme'] ?? 'light';
 <head>
     <meta charset="UTF-8">
     <title>Campus Connect 🌸</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 
     <!-- 🎀 Responsive design magic -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
